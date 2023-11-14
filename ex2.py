8016a986c5d85909154b1f011cf4ec8809eeae06
Roxane Breton <julie.roxane@outlook.fr>
2023-11-14 16:39:42 +0100

    Exercice 2

---
+++ ex2.py
@@ -0,0 +1,5 @@
+def displayStringUpper():
+    user = input ("Veuillez saissir une phrase : ")
+    print("Vous avez écris : " + user.upper() + ", voici votre phrase en petit : " + user.lower() )
+
+displayStringUpper()
